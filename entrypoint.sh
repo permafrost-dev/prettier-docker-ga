f43a7b205e1e781e26fd18720f8480e741460bc0
Patrick Organ <patrick@permafrost.dev>
2021-12-25 12:11:50 -0500

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ CONFIG_OPTION="--config /app/prettier.config.js"
 
 NEEDS_CONFIG_OPTION=$(echo "$*" | grep "\-\-config" | wc -l)
 
-if [ $NEEDS_CONFIG_OPTION -eq 0 ]; then
+if [ $NEEDS_CONFIG_OPTION -gt 0 ]; then
     CONFIG_OPTION=""
 fi
 
-CMD="prettier --write $CONFIG_OPTION $*"
+cd /github/workspace
 
 printf "prettier v"
 prettier --version
-prettier --write $*
+prettier --write --no-error-on-unmatched-pattern $CONFIG_OPTION $*
